d98802ad2fb432eb692b9a2138976c4c864431e2
Mahir Shadid <mahir.shadid@gmail.com>
2023-09-29 19:10:06 +0600

    Initialization

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.sass';
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
