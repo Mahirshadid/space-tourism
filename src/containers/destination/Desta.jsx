c43a80918ea14f5ce87ea2c19a1f36655cc8f83c
Mahir Shadid <mahir.shadid@gmail.com>
2023-09-30 21:15:22 +0600

    Update Desta.jsx

--- src/containers/destination/Desta.jsx
+++ src/containers/destination/Desta.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './desta.css'
 
 const Desta = () => {
   return (
-    <div className='desta section__padding'></div>
+    <div className='desta section__padding'>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Desta />} />
+          
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
